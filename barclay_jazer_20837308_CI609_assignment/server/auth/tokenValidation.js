910a268fc0e25f7848baacfd1a7c4220bac42a58
Jazer <contact@jazer.co.uk>
2022-11-14 12:41:16 +0000

    Added authentication middleware

---
+++ barclay_jazer_20837308_CI609_assignment/server/auth/tokenValidation.js
@@ -0,0 +1,26 @@
+const { verify } = require('jsonwebtoken');
+
+module.exports = {
+    validateToken: (req, res, next) => {
+        // Get token from header
+        let token = req.headers.authorization;
+        
+        if (!token) return res.status(401).json({ error: 'missing token' });
+
+        // Remove 'Bearer ' string before key
+        token = token.slice(7);
+
+        // Verify token using jwt
+        verify(token, process.env.JWT_KEY || 'secret', (err, decoded) => {
+
+            // If verification fails, return 403
+            if (err) return res.status(403).json({ error: 'invalid token' });
+
+            // Store authorization in request body as auth
+            req.body.auth = decoded;
+
+            // Go to next
+            return next();
+        });
+    }
+};
