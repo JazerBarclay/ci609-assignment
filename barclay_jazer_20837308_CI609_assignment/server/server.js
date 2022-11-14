f0e78669c171e7737779aac480a513c8c3532967
Jazer <contact@jazer.co.uk>
2022-11-14 14:03:22 +0000

    Added routes and dotenv config variables

--- barclay_jazer_20837308_CI609_assignment/server/server.js
+++ barclay_jazer_20837308_CI609_assignment/server/server.js
@@ -1,111 +1,48 @@
+// Import enviroment config
+require('dotenv').config();
+
+// Import express app
 const express = require('express');
-const fs = require('fs');
-const bodyParser = require('body-parser');
-const db = require('./db');
 
+// Init morgan logging
 const logger = require('morgan');
+const fs = require('fs');
 
+// Initialise express app
 const app = express();
-const port = process.env.PORT || 4000;
 
+// Set port to process variable or, if unset, to 4000
+const PORT = process.env.PORT || 4000;
+
+// Use morgan to log access to file access.log
 app.use(logger('common', {
-    stream: fs.createWriteStream('./access.log', {flags: 'a'})
+    stream: fs.createWriteStream(process.env.LOGFILE || './access.log', {flags: 'a'})
 }));
-// app.use(logger('tiny'));
 
-app.use(bodyParser.urlencoded({extended: false}));
+// Parsing the incoming data from either form or json format
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.get('/ci609/wk3/express-api', (req, res) => {
-    res.json({'message': 'ok'});
-})
+// Add signup route
+const signupRouter = require('./routes/signup/signupRouter');
+app.use('/signup', signupRouter);
 
-app.get('/ci609/wk3/express-api/express_api', async (req, res) => {
-    const {status, data} = await getComments(req);
-    res.status(status);
-    if(data) res.json(data);
-    else res.end();
-})
+// Add login route
+const loginRouter = require('./routes/login/loginRouter');
+app.use('/login', loginRouter);
 
-app.post('/ci609/wk3/express-api/express_api', async (req, res) => {
-    const {status, data} = await postComments(req);
-    res.status(status);
-    if(data) res.json(data);
-    else res.end();
-})
+// Add user route
+const userRouter = require('./routes/users/userRouter');
+app.use('/users', userRouter);
 
-app.put('/ci609/wk3/express-api/express_api', async (req, res) => {
-    res.status(405);
-    res.end();
-})
+// Add sightings
+const sightingsRouter = require('./routes/sightings/sightingRouter');
+app.use('/sightings', sightingsRouter);
 
-app.delete('/ci609/wk3/express-api/express_api', async (req, res) => {
-    res.status(405);
-    res.end();
-})
+// Add default route
+app.get('/', (req, res) => {
+    res.status(200).end();
+});
 
-async function getComments(req) {
-    let status = 500, data = null;
-    try {
-        const oid = req.query.oid;
-    
-        if(oid && oid.length > 0 && 
-            oid.length <= 32 && oid.match(/^[a-z0-9]+$/i)){
-   
-            const sql = 'SELECT name, comment FROM comments WHERE oid = ?';
-            const rows = await db.query(sql, [oid]);
-   
-            if(rows){
-                status = 200;
-                data = {
-                    'oid': oid,
-                    'comments': rows
-                };
-            } else {
-                status = 204;
-            }
-        } else {
-            status = 400;
-        }
-    } catch(e) {
-        console.error(e);
-    }
-    
-    return {status, data};
-}
-
-async function postComments(req) {
-    let status = 500, data = null;
-    try {
-        const oid = req.body.oid;
-        const name = req.body.name;
-        const comment = req.body.comment;
-
-        console.log(oid)
-
-        if(oid && name && comment
-            && oid.length > 0 && oid.length <= 32
-            && oid.match(/^[a-z0-9]+$/i)
-            && name.length > 0 && name.length <= 64
-            && comment.length > 0 ){
-
-            const sql = 'INSERT INTO comments (oid, name, comment) '
-            + 'VALUES (?, ?, ?)';
-            const result = await db.query(sql, [oid, name, comment]);
-        
-            if(result.affectedRows) {
-                status = 201;
-                data = {'id': result.insertId };
-            }
-        } else {
-            status = 400;
-        }
-    } catch(e) {
-        console.error(e);
-    }
-
-    return {status, data};
-}
-       
-
-app.listen(port, () => 
-    console.log(`Running at http://localhost:${port}`));
+// Start app
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
